3e6bf31fa2fc8963ea524c14df1dda57b50090a1
Home\Pc <drmakc@gmail.com>
2018-04-10 20:01:26 +0300

    pep 8 optimized, requests imported

--- classes_in_python.py
+++ classes_in_python.py
@@ -1,41 +1,54 @@
+import requests
+
+
 class Animal:
-	obey_humans = True
-	live_on_farm = True
-	legs = None
-	mouth = None
-	production = None
+    obey_humans = True
+    live_on_farm = True
+    legs = None
+    mouth = None
+    production = None
+
 
 class Even_toed(Animal):
-	fur = True
-	mouth = "lips"
-	legs = 4
+    fur = True
+    mouth = "lips"
+    legs = 4
+
 
 class Bird(Animal):
-	feathers = True
-	mouth = "beak"
-	legs = 2
-	wings = 2
+    feathers = True
+    mouth = "beak"
+    legs = 2
+    wings = 2
+
 
 class Cow(Even_toed):
-	production = ["milk", "meat"]
+    production = ["milk", "meat"]
+
 
 class Goat(Even_toed):
-	production = ["milk", "meat"]
+    production = ["milk", "meat"]
+
 
 class Sheep(Even_toed):
-	production = ["fur", "meat"]
+    production = ["fur", "meat"]
+
 
 class Pig(Even_toed):
-	production = ["meat"]
+    production = ["meat"]
+
 
 class Duck(Bird):
-	production = ["meat"]
+    production = ["meat"]
+
 
 class Chick(Bird):
-	production = ["eggs", "meat"]
+    production = ["eggs", "meat"]
+
 
 class Goose(Bird):
-	production = ["meat"]
+    production = ["meat"]
+
 
 jeffrey = Goose()
 print(jeffrey.feathers)
