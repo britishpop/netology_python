37544a61c4701612f99b9b324a485225ea67b97c
britishpop <drmakc@gmail.com>
2018-05-01 13:05:01 +0300

    seems done

--- 2.4_file_paths/2.3_find_inside.py
+++ 2.4_file_paths/2.3_find_inside.py
@@ -3,45 +3,37 @@ import os
 
 def main():
     answer = None
+    search_memory = []
     while answer is not "q":
-        answer = input("Welcome, stranger! Got many good things on sale! What are you buying? \n Or you could always "
-                       "input q to exit ")
-        found_keys = find_keys(answer)
+        answer = input("Введите значение для поиска или нажмите q, чтобы выйти.\nВнимание! Вторая и последующие "
+                       "итерации поиска будут проходить среди уже отобранных файлов ")
+        found_keys = find_keys(answer, search_memory)
         if len(found_keys) > 1:
-            for one_line in enumerate(found_keys):
-                print("Документов с этим значением много. Смотрите:\n {}".format(one_line))
-                find_keys(answer)
+            print("Вот в этих файлах есть {}:".format(answer))
+            for i, single_key in enumerate(found_keys):
+                print(i + 1, single_key)
+            search_memory = found_keys
         elif len(found_keys) == 1:
-            print("Это единственный файл с таким значением. На этом наши полномочия все: \n {}".format(found_keys[1]))
+            print("Нашелся всего один файл, содержащий {}".format(answer))
         else:
-            print("Better luck next time. We found nothing")
-            break
+            print("Кажется, ничего не нашлось. Попробуйте поискать что-то другое\n")
 
 
-def find_keys(answer):
+def find_keys(answer, search_memory):
     current_dir = os.path.dirname(os.path.abspath(__file__))
     iteration_list = []
-    for i, listed_file in enumerate(os.listdir(os.path.join(current_dir, "migrations"))):
-        if listed_file.endswith(".sql"):
-            with open(os.path.join(current_dir, "migrations", listed_file)) as f:
+    if len(search_memory) is 0:
+        for listed_file in os.listdir(os.path.join(current_dir, "migrations")):
+            if listed_file.endswith(".sql"):
+                with open(os.path.join(current_dir, "migrations", listed_file)) as f:
+                    if answer in f.read():
+                        iteration_list.append(f.name)
+    else:
+        for previously_found in search_memory:
+            with open(previously_found) as f:
                 if answer in f.read():
-                    iteration_list.append(f)
+                    iteration_list.append(f.name)
     return iteration_list
 
 
-# how to list all files with *.sql?
-# listed all files with *.sql
-# how to get all file names of *.sql files
-# read file with a for cycle
-# if item in file
-# append filename to list
-# substitute filename from list to os.path.join
-# repeat search
-# print list
-
-
 main()
-
-# if __name__ == '__main__':
-#     # ваша логика
-#     pass
