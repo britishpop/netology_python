9593dd42a0f7b11b97dd9e3c06cb0d6d78c1b9fc
britishpop <drmakc@gmail.com>
2018-05-07 19:00:27 +0300

    it works

--- 2.5_external_programs/2.5_call_sis_to_resize.py
+++ 2.5_external_programs/2.5_call_sis_to_resize.py
@@ -1,18 +1,20 @@
 import os
+import shutil
 import subprocess
 
 
 def resize():
     current_dir = os.path.dirname(os.path.abspath(__file__))
     ensure_dir(current_dir)
-    for picture in os.listdir(os.path.join(current_dir, "source")):
-        subprocess.call("cp {} {}".format(os.path.join(current_dir, picture), os.path.join(current_dir, "result",
-                                                                                           picture)))
-    for picture in os.listdir(os.path.join(current_dir, "result")):
+    source_dir = os.path.join(current_dir, "source")
+    result_dir = os.path.join(current_dir, "result")
+    for picture in os.listdir(source_dir):
         if picture.endswith(".jpg"):
-            subprocess.call(["sips", "--resampleWidth", "200", os.path.join(current_dir, "result", picture)])
+            shutil.copyfile(os.path.join(source_dir, picture), os.path.join(result_dir, picture))
         else:
             print("{} это не картинка".format(picture))
+    for picture in os.listdir(result_dir):
+        subprocess.call(["sips", "--resampleWidth", "200", os.path.join(result_dir, picture)])
 
 
 def ensure_dir(file_path):
