4a25f6c95acff267ce5a900ca19d62a234347057
britishpop <drmakc@gmail.com>
2018-06-06 10:41:32 +0300

    added real credentials added comparison added community info query

--- 6.0_diploma/6.0_diploma_logic.py
+++ 6.0_diploma/6.0_diploma_logic.py
@@ -1,3 +1,4 @@
+# задача https://docs.google.com/document/d/1sDD59rDrrqzjLu_fZKpXlnig05RUW_YH5PUpKmeyG8w/edit
 # 1. Получить токен ВК
 # 2. Подключиться к API ВК
 # 3. Получить список групп пользователя
@@ -13,14 +14,26 @@
 # 6.1 Каждый раз при сравнении удалять из массива пользователя совпавшие элементы
 # 6.1.1 Возможно постоянно сохранять новый массив несовпавших групп и сравнивать
 # только его
+# 7. получить информацию об уникальных для пользователя сообществах
+# 8. записать полученную информацию в json
 
 
 import requests
+
+import time
+
+import json
 
 from pprint import pprint
 
 from diploma_data import ACC_TOKEN, TARGET_VK
 
+
+test_set = set([33938434, 24112387, 53280908, 9624588, 15010187, 130785301,
+               124123414, 22440983, 23750039, 47855390, 75185191, 32174764,
+                40599470, 28140719, 72326580, 23391543, 16984508, 92307020,
+                 30786903, 45491419, 41960539, 41783644, 25205856, 92337511,
+                  1210474, 18133228, 22898541, 98748657])
 
 def get_user_communities(token, vkid):
     print("Запрос списка сообществ пользователя {}".format(vkid))
@@ -29,11 +42,26 @@ def get_user_communities(token, vkid):
         params=dict(
             access_token=token,
             v="5.75",
-            user_id=vkid
+            user_id=vkid,
+            count="50"
         )
     )
     return communities_list.json()
 
+
+def get_community_info(token, *args):
+    print(args)
+    print("Запрос информации об уникальных сообществах")
+    community_info = requests.get(
+        "https://api.vk.com/method/groups.get",
+        params=dict(
+            access_token=token,
+            v="5.75",
+            group_ids=args,
+            fields="id, name, members_count"
+        )
+    )
+    return communities_info.json()
 
 def get_user_friends(token, vkid):
     print("Запрос списка друзей пользователя {}".format(vkid))
@@ -42,7 +70,8 @@ def get_user_friends(token, vkid):
         params=dict(
             access_token=token,
             v="5.75",
-            user_id=vkid
+            user_id=vkid,
+            count="50"
         )
     )
     return friends_list.json()
@@ -52,19 +81,20 @@ def compare_communities(token, vkid):
     user_communities = set(get_user_communities(token, vkid)["response"]["items"])
     user_friends = list(get_user_friends(token, vkid)["response"]["items"])
     user_friends_communities = []
+    counter = 1
     for friend in user_friends:
         try:
             user_friends_communities.append(set(
                 get_user_communities(token, friend)["response"]["items"]))
+            time.sleep(0.38)
         except KeyError:
             print("Запрос сообществ пользователя {} завершился неудачей.\n"
                   "Возможно, настройки приватности".format(friend))
             continue
-    pprint(user_friends_communities)
-    # for i, community_set in enumerate(user_friends_communities), 1:
-    #     print("Сравнение списков сообществ, шаг {}".format(i))
-    #     user_communities.different(community_set) # TODO: add set method for comparison
+    for community_set in user_friends_communities:
+        print("Сравнение списков сообществ, шаг {}".format(counter))
+        user_communities -= community_set
+        counter += 1
+    return user_communities
 
-# pprint(get_user_communities(ACC_TOKEN, TARGET_VK)["response"]["items"])
-
-pprint(compare_communities(ACC_TOKEN, TARGET_VK))
+pprint(get_community_info(ACC_TOKEN, test_set))
